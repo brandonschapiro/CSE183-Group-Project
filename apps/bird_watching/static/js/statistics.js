9d6002a9a3a66b3afa9886ff2c243c386bf30dd6
alitaquie <123027814+alitaquie@users.noreply.github.com>
2024-06-02 19:13:29 -0700

    connected stats front to back

--- apps/bird_watching/static/js/statistics.js
+++ apps/bird_watching/static/js/statistics.js
@@ -4,29 +4,35 @@
 // and be used to initialize it.
 let app = {};
 
+app.data = {
+  data: function() {
+    return {
+      // Complete as you see fit.
+      my_value: 1, // This is an example.
+      species_names: [],
+      observation_dates: [],
+    };
+  },
+  methods: {
+    // Complete as you see fit.
+  },
 
-app.data = {    
-    data: function() {
-        return {
-            // Complete as you see fit.
-            my_value: 1, // This is an example.
-        };
-    },
-    methods: {
-        // Complete as you see fit.
-        my_function: function() {
-            // This is an example.
-            this.my_value += 1;
-        },
-    }
 };
+
 
 app.vue = Vue.createApp(app.data).mount("#app");
 
-app.load_data = function () {
-    axios.get(my_callback_url).then(function (r) {
-        app.vue.my_value = r.data.my_value;
-    });
-}
+app.load_data = function() {
+  axios.get(get_user_statistics_url).then(function(r) {
+    app.vue.species_names = r.data.species_names;
+    app.vue.observation_dates = r.data.observation_dates;
+    
+    // Print the first 10 names and dates to console
+    console.log("First 10 names and dates:");
+    for (let i = 0; i < 10 && i < r.data.species_names.length; i++) {
+      console.log(r.data.species_names[i], r.data.observation_dates[i]);
+    }
+  });
+};
 
 app.load_data();
