458dff39214b50298d340c2a2f9663dfaf3ab0ca
Adarsh Pinjala <apinjala@ucsc.edu>
2024-06-08 12:35:33 -0700

    increment count

--- apps/bird_watching/static/js/checklist.js
+++ apps/bird_watching/static/js/checklist.js
@@ -2,66 +2,78 @@
 
 let app = {};
 
+// Vue.js application data and methods
 app.data = {
     data() {
         return {
-            searchTerm: '', // Term to search for species
-            speciesResults: [], // Results from species search
-            sightings: [], // List of species sightings
-            latitude: null, // Latitude for the observation location
-            longitude: null // Longitude for the observation location
+            searchTerm: '', // The term to search for species
+            speciesResults: [], // Array to store the results of the species search
+            sightings: [], // Array to store the species sightings
+            latitude: null, // Latitude of the observation location
+            longitude: null // Longitude of the observation location
         };
     },
     methods: {
-        // Search for species with a debounced function to limit API calls
+        // Method to search for species using a debounced function to limit the number of API calls
         searchSpecies: _.debounce(function() {
             axios.get(get_species_url, { params: { term: this.searchTerm }}).then((response) => {
-                this.speciesResults = response.data.species;
+                this.speciesResults = response.data.species; // Update species results with the response data
             });
         }, 300),
-        // Add a selected species to the sightings list
+        
+        // Method to add a species to the sightings list
         addSpecies(species) {
             this.sightings.push({
                 id: species.id,
                 name: species.name,
-                species_count: 1
+                species_count: 1 
             });
-            this.speciesResults = [];
-            this.searchTerm = '';
+            this.speciesResults = []; 
+            this.searchTerm = ''; 
         },
-        // Remove a species from the sightings list
+        
+        // Method to remove a species from the sightings list
         removeSpecies(index) {
-            this.sightings.splice(index, 1);
+            this.sightings.splice(index, 1); 
         },
-        // Submit the checklist to the server
+        
+        // Method to increment the count of a species in the sightings list
+        incrementCount(index) {
+            this.sightings[index].species_count++; 
+        },
+        
+        // Method to submit the checklist
         submitChecklist() {
             if (this.latitude === null || this.longitude === null) {
-                alert("Please select a location on the map.");
+                alert("Please select a location on the map."); 
                 return;
             }
 
             let data = {
                 latitude: this.latitude,
                 longitude: this.longitude,
-                observation_date: new Date().toISOString().split('T')[0],
-                observation_time: new Date().toISOString().split('T')[1].split('.')[0],
-                observation_duration: 60,  // Sample duration in minutes
-                sightings: this.sightings
+                observation_date: new Date().toISOString().split('T')[0], 
+                observation_time: new Date().toISOString().split('T')[1].split('.')[0], 
+                observation_duration: 60, 
+                sightings: this.sightings 
             };
+
+            // Post the checklist data to the server
             axios.post(submit_checklist_url, data).then((response) => {
                 if (response.data.status === 'success') {
-                    alert("Checklist submitted successfully!");
-                    this.sightings = [];
+                    alert("Checklist submitted successfully!"); 
+                    this.sightings = []; 
                 }
             });
         }
     },
+    
+    // Mounted lifecycle hook to set the initial location
     mounted() {
-        // Set default location coordinates; ideally, this should be set based on user interaction on the map
-        this.latitude = 37.7749;
-        this.longitude = -122.4194;
+        this.latitude = 37.7749; 
+        this.longitude = -122.4194; 
     }
 };
 
-// Initialize Vue app with the specified data and methods
+// Create and mount the Vue.js application
 app.vue = Vue.createApp(app.data).mount("#app");
