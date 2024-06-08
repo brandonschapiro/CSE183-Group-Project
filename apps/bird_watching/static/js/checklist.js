a92e093411c91936da2ab8fb5538c0a666e8842d
Adarsh Pinjala <apinjala@ucsc.edu>
2024-06-08 14:58:10 -0700

    updated checklist.js

--- apps/bird_watching/static/js/checklist.js
+++ apps/bird_watching/static/js/checklist.js
@@ -2,78 +2,74 @@
 
 let app = {};
 
-// Vue.js application data and methods
 app.data = {
-    data() {
-        return {
-            searchTerm: '', // The term to search for species
-            speciesResults: [], // Array to store the results of the species search
-            sightings: [], // Array to store the species sightings
-            latitude: null, // Latitude of the observation location
-            longitude: null // Longitude of the observation location
-        };
-    },
-    methods: {
-        // Method to search for species using a debounced function to limit the number of API calls
-        searchSpecies: _.debounce(function() {
-            axios.get(get_species_url, { params: { term: this.searchTerm }}).then((response) => {
-                this.speciesResults = response.data.species; // Update species results with the response data
-            });
-        }, 300),
-        
-        // Method to add a species to the sightings list
-        addSpecies(species) {
-            this.sightings.push({
-                id: species.id,
-                name: species.name,
-                species_count: 1 
-            });
-            this.speciesResults = []; 
-            this.searchTerm = ''; 
-        },
-        
-        // Method to remove a species from the sightings list
-        removeSpecies(index) {
-            this.sightings.splice(index, 1); 
-        },
-        
-        // Method to increment the count of a species in the sightings list
-        incrementCount(index) {
-            this.sightings[index].species_count++; 
-        },
-        
-        // Method to submit the checklist
-        submitChecklist() {
-            if (this.latitude === null || this.longitude === null) {
-                alert("Please select a location on the map."); 
-                return;
-            }
-
-            let data = {
-                latitude: this.latitude,
-                longitude: this.longitude,
-                observation_date: new Date().toISOString().split('T')[0], 
-                observation_time: new Date().toISOString().split('T')[1].split('.')[0], 
-                observation_duration: 60, 
-                sightings: this.sightings 
-            };
-
-            // Post the checklist data to the server
-            axios.post(submit_checklist_url, data).then((response) => {
-                if (response.data.status === 'success') {
-                    alert("Checklist submitted successfully!"); 
-                    this.sightings = []; 
-                }
-            });
-        }
-    },
-    
-    // Mounted lifecycle hook to set the initial location
-    mounted() {
-        this.latitude = 37.7749; 
-        this.longitude = -122.4194; 
+  data: function() {
+    return {
+      search: "",
+      speciesList: [],
+      checklists: [],
+      selectedSpecies: [],
+      lat: null,
+      lng: null
+    };
+  },
+  computed: {
+    filteredSpecies: function() {
+      return this.speciesList.filter(species =>
+        species.name.toLowerCase().includes(this.search.toLowerCase())
+      );
     }
+  },
+  methods: {
+    fetchSpecies: function() {
+      axios.get(get_species_url).then(response => {
+        this.speciesList = response.data.species.map(species => ({
+          ...species,
+          count: 0
+        }));
+      });
+    },
+    fetchChecklists: function() {
+      axios.get(get_checklists_url).then(response => {
+        this.checklists = response.data.checklists;
+      });
+    },
+    incrementCount: function(species) {
+      species.count += 1;
+    },
+    submitChecklist: function() {
+      const checklistData = {
+        latitude: this.lat,
+        longitude: this.lng,
+        species: this.selectedSpecies.filter(species => species.count > 0)
+      };
+      axios.post(submit_checklist_url, checklistData).then(response => {
+        this.fetchChecklists();
+        this.selectedSpecies.forEach(species => (species.count = 0));
+      });
+    },
+    editChecklist: function(checklist) {
+      window.location.href = `/bird_watching/checklist?checklist_id=${checklist.id}`;
+    },
+    deleteChecklist: function(checklistId) {
+      axios.post(delete_checklist_url, { id: checklistId }).then(response => {
+        this.fetchChecklists();
+      });
+    }
+  },
+  mounted() {
+    const urlParams = new URLSearchParams(window.location.search);
+    this.lat = urlParams.get("lat");
+    this.lng = urlParams.get("lng");
+    this.fetchSpecies();
+    this.fetchChecklists();
+  }
 };
 
-// Create and mount the Vue.js application
 app.vue = Vue.createApp(app.data).mount("#app");
+
+app.init = function() {
+  app.vue;
+};
+
+app.init();
